2194ef719d55acd395664fc6aa90a528058c7ce8
ycalk <alosev752@gmail.com>
2025-07-13 21:30:48 +0500

    Add opportunity to create checkpoints while training

--- additional/research/src/research/utils/trainer.py
+++ additional/research/src/research/utils/trainer.py
@@ -1,6 +1,7 @@
+import torch
+import os
 from typing import NamedTuple
 from numpy import ndarray
-import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from .dataset import MergeDataset
@@ -26,6 +27,7 @@ class Trainer:
         self,
         model: nn.Module,
         dataset: MergeDataset,
+        checkpoint_dir: str,
         optimizer: optim.Optimizer | None = None,
         lr: float = 0.001,
         epochs: int = 10,
@@ -60,6 +62,10 @@ class Trainer:
         self.criterion = criterion
         self.epochs = epochs
         self.device = device
+        self.checkpoint_dir = checkpoint_dir
+        self.result: list[EpochResult] = []
+        self.current_epoch = 0
+        self.load_checkpoint()
 
     def __call__(self) -> list[EpochResult]:
         """Запуск обучения модели на заданном датасете.
@@ -68,13 +74,12 @@ class Trainer:
             list[EpochResult]: Список результатов по эпохам, содержащий потери, точность и матрицу ошибок
                 на train и val наборах данных.
         """
-        result: list[EpochResult] = []
 
-        for epoch in range(self.epochs):
+        for self.current_epoch in range(self.current_epoch, self.epochs):
             train_loss, train_accuracy, train_cm = self._run_epoch(False)
             val_loss, val_accuracy, val_cm = self._run_epoch(True)
 
-            result.append(
+            self.result.append(
                 EpochResult(
                     train_loss=train_loss,
                     train_accuracy=train_accuracy,
@@ -84,19 +89,21 @@ class Trainer:
                     val_cm=val_cm,
                 )
             )
-            
+
             torch.cuda.empty_cache()
             torch.cuda.ipc_collect()
-            
+
             print(
-                f"Epoch {epoch + 1}/{self.epochs} - "
+                f"Epoch {self.current_epoch + 1}/{self.epochs} - "
                 f"Train Loss: {train_loss:.4f}, "
                 f"Train Accuracy: {train_accuracy:.4f}, "
                 f"Val Loss: {val_loss:.4f}, "
                 f"Val Accuracy: {val_accuracy:.4f}"
             )
 
-        return result
+            self.create_checkpoint()
+
+        return self.result
 
     def _run_epoch(self, validate: bool) -> tuple[float, float, ndarray]:
         """Выполнение одной эпохи обучения или валидации модели.
@@ -149,3 +156,33 @@ class Trainer:
             correct / total,
             confusion_matrix(all_targets, all_preds),
         )
+
+    def create_checkpoint(self) -> None:
+        """Сохранение текущего состояния модели и оптимизатора."""
+        os.makedirs(self.checkpoint_dir, exist_ok=True)
+        model_path = os.path.join(self.checkpoint_dir, "checkpoint.pth")
+        torch.save(
+            {
+                "model_state_dict": self.model.state_dict(),
+                "optimizer_state_dict": self.optimizer.state_dict(),
+                "result": self.result,
+                "epochs": self.current_epoch,
+            },
+            model_path,
+        )
+        print(f"Model checkpoint saved to {model_path}")
+
+    def load_checkpoint(self) -> None:
+        """Загрузка модели из сохраненного состояния."""
+        model_path = os.path.join(self.checkpoint_dir, "checkpoint.pth")
+        if os.path.exists(model_path):
+            checkpoint = torch.load(
+                model_path, map_location=self.device, weights_only=False
+            )
+            self.model.load_state_dict(checkpoint["model_state_dict"])
+            self.optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+            self.result = checkpoint["result"]
+            self.current_epoch = checkpoint["epochs"]
+            print(f"Model loaded from {model_path}")
+        else:
+            print(f"No saved model found at {model_path}")
