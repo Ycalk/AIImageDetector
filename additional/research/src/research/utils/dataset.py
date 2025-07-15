54c52ee19e4e3cd7cd87eda4924bef46f543b6db
ycalk <alosev752@gmail.com>
2025-07-15 20:27:21 +0500

    Add new research iteration result update resnet model

--- additional/research/src/research/utils/dataset.py
+++ additional/research/src/research/utils/dataset.py
@@ -50,7 +50,7 @@ class ArtiFactDataset(VisionDataset):
                 [
                     transforms.Resize((256, 256)),
                     transforms.ToTensor(),
-                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
                 ]
             ),
             images_count=train_size,
@@ -62,7 +62,7 @@ class ArtiFactDataset(VisionDataset):
                 [
                     transforms.Resize((256, 256)),
                     transforms.ToTensor(),
-                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
                 ]
             ),
             images_count=val_size,
--- additional/research/src/research/utils/res_net_model.py
+++ additional/research/src/research/utils/res_net_model.py
@@ -6,11 +6,26 @@ import torch
 class ResNetModel(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
-        self.model.fc = nn.Linear(512, 1)
+        self.features = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+        self.features.fc = nn.Identity() # type: ignore
+        self.fc = nn.Sequential(
+            nn.Linear(512, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, 1),
+        )
+
+    def freeze_features(self) -> None:
+        for param in self.features.parameters():
+            param.requires_grad = False
+
+    def unfreeze_features(self) -> None:
+        for param in self.features.parameters():
+            param.requires_grad = True
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.model(x)
+        features = self.features(x)
+        return self.fc(features)
 
 
 if __name__ == "__main__":
