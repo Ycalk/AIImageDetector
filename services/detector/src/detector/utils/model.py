f38c287e1476519105625c3e94dd2c2b0b971369
ycalk <alosev752@gmail.com>
2025-07-17 00:27:13 +0500

    Fix image pretransform in detector

--- services/detector/src/detector/utils/model.py
+++ services/detector/src/detector/utils/model.py
@@ -11,7 +11,7 @@ class Model:
             [
                 transforms.ToTensor(),
                 transforms.Resize((256, 256)),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
             ]
         )
 
